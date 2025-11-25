a41340f5ee5a125e886f72a3e789b49a66e85575
princesscheryl <donkor.princesscheryl@gmail.com>
2025-11-25 13:42:34 +0000

    debugging the admin dashboard

--- admin/platform_dashboard.php
+++ admin/platform_dashboard.php
@@ -1,4 +1,8 @@
 <?php
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once 'includes_platform/auth_check.php';
 require_once '../classes/admin_class.php';
 
