d85e9bc84a212d2560e49e60fb8de4478a35c258
princesscheryl <donkor.princesscheryl@gmail.com>
2025-11-29 23:46:53 +0000

    sidebar on reviews page

--- view/provider/manage_reviews.php
+++ view/provider/manage_reviews.php
@@ -61,6 +61,7 @@ $current_page = 'reviews';
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
+    <link href="../../css/provider_sidebar.css" rel="stylesheet">
     <link href="../../css/provider_reviews.css" rel="stylesheet">
 </head>
 <body>
