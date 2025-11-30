d811667ca1bcc097c185c6cfca6d063be8fa8df5
princesscheryl <donkor.princesscheryl@gmail.com>
2025-11-30 21:10:48 +0000

    properly formatted add service page

--- admin/add_service.php
+++ admin/add_service.php
@@ -38,6 +38,7 @@ $current_page = 'add_service';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="../css/provider_sidebar.css" rel="stylesheet">
     <link href="../css/add_service.css" rel="stylesheet">
 </head>
 <body>
